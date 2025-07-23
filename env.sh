d898fc95557a75a602e527d5dfd826feac37a9e7
SwissTao <SwissTao@node0.swisstao-263921.beacon-pg0.cloudlab.umass.edu>
2025-07-23 09:35:58 -0600

    update

--- env.sh
+++ env.sh
@@ -4,29 +4,31 @@ sudo apt-get install -y dotnet-sdk-8.0
 
 # Download Dafny, Linux kernel, and VM image
 
-pushd fuzzing-dir
-wget --no-check-certificate  https://zenodo.org/records/16366262/files/dafny.tar.gz?download=1 -O dafny.tar.gz
-tar -xzvf dafny.tar.gz
-
-wget --no-check-certificate  https://zenodo.org/records/16366262/files/linux-kernel.tar.gz?download=1 -O linux-kernel.tar.gz
-tar -xzvf linux-kernel.tar.gz
-
-wget --no-check-certificate https://zenodo.org/records/16366262/files/vm-image.tar.gz?download=1 -O vm-image.tar.gz
-tar -xzvf vm-image.tar.gz
-popd
+# pushd fuzzing-dir
+# wget --no-check-certificate  https://zenodo.org/records/16366262/files/dafny.tar.gz?download=1 -O dafny.tar.gz
+# tar -xzvf dafny.tar.gz
+# 
+# wget --no-check-certificate  https://zenodo.org/records/16366262/files/linux-kernel.tar.gz?download=1 -O linux-kernel.tar.gz
+# tar -xzvf linux-kernel.tar.gz
+# 
+# wget --no-check-certificate https://zenodo.org/records/16366262/files/vm-image.tar.gz?download=1 -O vm-image.tar.gz
+# tar -xzvf vm-image.tar.gz
+# popd
 
 
 # fatal error: sys/capability.h: No such file or directory 
-sudo apt-get install libcap-dev
-
+sudo apt-get install -y libcap-dev
+sudo apt-get install -y libelf-dev
+sudo apt-get install -y libzstd-dev
 
 # install libbpf: https://github.com/libbpf/libbpf
 git clone https://github.com/libbpf/libbpf
-cd libbpf
-cd src
+pushd libbpf
+pushd src
 make -j`nproc`
 sudo make install
-
+popd
+popd
 
 # Install go
 mkdir go-env
